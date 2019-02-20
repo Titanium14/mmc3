0e0ed3ef2e800b3bc8844f70c9d02fdd20d848d1
Wai Jyuen Phang <phang.wj@gmail.com>
2019-02-20 03:09:35 +0000

    Completed backend functionality. Now moving onto frontend development. Also added a ESLint file to encourage good coding practises.

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://titan:titan1@ds135255.mlab.com:35255/my-money-counts"
+  mongoURI: 'mongodb://titan:titan1@ds135255.mlab.com:35255/my-money-counts',
+  secretOrKey: 'secret'
 };
--- server.js
+++ server.js
@@ -1,27 +1,37 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const passport = require('passport');
 
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
 // DB config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("MongoDB connected"))
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello!"));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
 
 // Use routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
